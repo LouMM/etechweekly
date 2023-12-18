ef4be23e52cb3d63589f1279e2201c84d6fb685e
LouMM <lmaresca@outlook.com>
2023-12-17 21:33:11 -0500

    Updating gatsby.config, removing mailchimp

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,12 +114,6 @@ module.exports = {
       resolve: `gatsby-plugin-lodash`,
     },
     {
-      resolve: 'gatsby-plugin-mailchimp',
-      options: {
-        endpoint: process.env.MAILCHIMP_ENDPOINT, // add your MC list endpoint here; see instructions below
-      },
-    },
-    {
       resolve: `gatsby-plugin-web-font-loader`,
       options: {
         google: {
